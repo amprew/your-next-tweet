864cec2da283bebe0283d624c3b4da13e0e02c0e
Andrew Collins <me@andrewww.co.uk>
2018-03-04 12:37:23 +0000

    Add alias for props user

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,20 +5,24 @@ export default class Tweet extends React.Component {
     super(props);
   }
 
+  get user() {
+    return this.props.user;
+  }
+
   render() {
     return (
       <blockquote className="tweet">
         <div className="tweet__main">
-          <a className="tweet__header" href="#">
+          <a href={`//twitter.com/${this.user.screen_name}`} className="tweet__header" target="_blank">
             <span className="tweet__author-avatar">
-              <img alt="tweet avatar" src={this.props.user.profile_image_url}/>
+              <img alt="tweet avatar" src={this.user.profile_image_url}/>
             </span>
-            <div className="tweet__author-name">{this.props.user.name}</div>
-            <div className="tweet__author-handle" title={`@${this.props.user.screen_name}`}>@{this.props.user.screen_name}</div>
+            <div className="tweet__author-name">{this.user.name}</div>
+            <div className="tweet__author-handle" title={`@${this.user.screen_name}`}>@{this.user.screen_name}</div>
           </a>
           <div className="tweet__body">
             <p className="tweet__body-text">{this.props.text}</p>
-            <p className="tweet__timestamp"><time datetime="2018-03-03T19:50">19:50 - 03 March 2018</time></p>
+            <p className="tweet__timestamp"><time datetime="now">now</time></p>
           </div>
         </div>
       </blockquote>
